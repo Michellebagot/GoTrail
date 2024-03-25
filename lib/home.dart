42def5f7df68455c90611666d970e865969ffd86
arpstha <shrestharp88@gmail.com>
2024-03-25 12:29:27 +0000

    refactored profile page

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/profile/profile_page.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,28 +15,7 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute<ProfileScreen>(
-                  builder: (context) => ProfileScreen(
-                    appBar: AppBar(
-                      title: const Text('User Profile'),
-                    ),
-                    actions: [
-                      SignedOutAction((context) {
-                        Navigator.of(context).pop();
-                      })
-                    ],
-                    children: [
-                      const Divider(),
-                      Padding(
-                        padding: const EdgeInsets.all(2),
-                        child: AspectRatio(
-                          aspectRatio: 1,
-                          child: Image.asset('flutterfire_300x.png'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                profilePage(),
               );
             },
           )
@@ -45,9 +25,9 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Image.asset('dash.png'),
+            // Image.asset('dash.png'),
             Text(
-              'Welcome!',
+              'This is the Home Screen of the App!',
               style: Theme.of(context).textTheme.displaySmall,
             ),
             const SignOutButton(),
@@ -56,4 +36,6 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
+
+
 }
