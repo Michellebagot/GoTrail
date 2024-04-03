78f24fabd43fb10d9c142ff77181709eb6107f82
eleanor-rf <eleanorfrank@outlook.com>
2024-04-03 09:34:45 +0100

    fixed routing for most cases

--- lib/header_bar/header_bar.dart
+++ lib/header_bar/header_bar.dart
@@ -4,11 +4,13 @@ import 'package:GoTrail/trail_blazer_options.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:GoTrail/home.dart';
 import 'package:GoTrail/map_view/map_page.dart';
 import 'package:GoTrail/trail_search/trail_search.dart';
 
-PreferredSizeWidget header(BuildContext context, BoxConstraints constraints, double shrinkOffset) {
+PreferredSizeWidget header(
+    BuildContext context, BoxConstraints constraints, double shrinkOffset) {
   return AppBar(
     title: GestureDetector(
       onTap: () {
@@ -26,7 +28,7 @@ PreferredSizeWidget header(BuildContext context, BoxConstraints constraints, dou
         ),
       ),
     ),
-      leading: Builder(
+    leading: Builder(
       builder: (BuildContext context) {
         if (ModalRoute.of(context)?.canPop ?? false) {
           return IconButton(
@@ -51,61 +53,81 @@ PreferredSizeWidget header(BuildContext context, BoxConstraints constraints, dou
     toolbarHeight: 50,
     backgroundColor: Color.fromRGBO(166, 132, 119, 1),
     actions: [
-      if (FirebaseAuth.instance.currentUser != null) IconButton(
-        icon: const Icon(Icons.pets),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => TrailBlazers(),
-            ),
-          );
-        },
-      ),
-       if (FirebaseAuth.instance.currentUser != null) IconButton(
-        icon: const Icon(Icons.local_fire_department_outlined),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => TrailBlazerProfile(),
-            ),
-          );
-        },
-      ),
-      if (FirebaseAuth.instance.currentUser != null) IconButton(
-        icon: const Icon(Icons.map),
-        onPressed: () {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => MapPage(),
-            ),
-          );
-        },
-      ),
-      if (FirebaseAuth.instance.currentUser != null) IconButton(
-        icon: const Icon(Icons.search),
-        onPressed: () {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => SearchPage(),
-            ),
-          );
-        },
-      ),
-      if (FirebaseAuth.instance.currentUser != null) IconButton(
-        icon: const Icon(Icons.person),
-        onPressed: () {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ProfilePage(),
-            ),
-          );
-        },
-      ),
+      if (FirebaseAuth.instance.currentUser != null)
+        StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(FirebaseAuth.instance.currentUser!.uid)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var trailBlazer = snapshot.data!['trailBlazer'];
+              if (trailBlazer == null || trailBlazer == '') {
+                return IconButton(
+                  icon: const Icon(Icons.pets),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TrailBlazers(),
+                      ),
+                    );
+                  },
+                );
+              } else {
+                return IconButton(
+                  icon: const Icon(Icons.local_fire_department_outlined),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TrailBlazerProfile(),
+                      ),
+                    );
+                  },
+                );
+              }
+            } else {
+              return SizedBox.shrink();
+            }
+          },
+        ),
+      if (FirebaseAuth.instance.currentUser != null)
+        IconButton(
+          icon: const Icon(Icons.map),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MapPage(),
+              ),
+            );
+          },
+        ),
+      if (FirebaseAuth.instance.currentUser != null)
+        IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SearchPage(),
+              ),
+            );
+          },
+        ),
+      if (FirebaseAuth.instance.currentUser != null)
+        IconButton(
+          icon: const Icon(Icons.person),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProfilePage(),
+              ),
+            );
+          },
+        ),
     ],
   );
 }
--- lib/trail_blazer_options.dart
+++ lib/trail_blazer_options.dart
@@ -13,109 +13,118 @@ class TrailBlazersState extends State<TrailBlazers> {
 
   List<QueryDocumentSnapshot> data = [];
 
+  @override
+  void initState() {
+    super.initState();
+  }
+
   var chosenTrailBlazer = '';
   var trailBlazerName = '';
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(         
-     body: SingleChildScrollView(
-                child: Column(
-                          children: [
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(children: [
       Padding(
-       padding: const EdgeInsets.all(25.0),
-       child: Column(
-         crossAxisAlignment: CrossAxisAlignment.stretch,
-        
-         children: [
-           Container(
-             padding: const EdgeInsets.all(5),
-             child: Center(child: Text('Choose your TrailBlazer', style: TextStyle(fontSize: 20),))
-             ),
-           Row(
-             children: [
-               Expanded(
-                 child: SizedBox(
-                   child: IconButton(
-                     icon: Image.asset('assets/blue_type1.gif'),
-                     iconSize: 1,
-                     onPressed: () {
-                       chosenTrailBlazer = 'water1';
-                       },
-                     ),
-                 )
-               ),
-               Expanded(
-                child: SizedBox( height: 250, width: 250,
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+                padding: const EdgeInsets.all(5),
+                child: Center(
+                    child: Text(
+                  'Choose your TrailBlazer',
+                  style: TextStyle(fontSize: 20),
+                ))),
+            Row(
+              children: [
+                Expanded(
+                    child: SizedBox(
                   child: IconButton(
-                   icon: Image.asset('assets/green_type1.gif'),
-                   iconSize: 1,
-                   onPressed: () {
-                     chosenTrailBlazer = 'grass1';
-                   },
-                 ),
-                )
-               ),
-             ],
-           ),
-           Container(
-             padding: const EdgeInsets.all(5),
-             child: SizedBox( height: 250, width: 250,
-               child: IconButton(
-                 icon: Image.asset('assets/brown_type1.gif'),
-                 iconSize: 50,
-                 onPressed: () {
-                     chosenTrailBlazer = 'mountain1';
-                 },
-               ),
-             )
-           ),
-           Center(
-             child: Text('Give your TrailBlazer a name:', style: TextStyle(fontSize: 20),)),
-           TextField(
-             onChanged:(value){
-               trailBlazerName = value;
-             },
-           decoration: InputDecoration(
-             border: OutlineInputBorder(),
-             hintText: 'Enter name here',
-            
-           ),
-           ),
-           Padding(
-             padding: const EdgeInsets.all(8.0),
-             child: ElevatedButton(
-               onPressed: (){
-                 if (chosenTrailBlazer.isEmpty || trailBlazerName.isEmpty){
-                 showAlertDialog(context);
-
-
-                 }else{
-                 final userUpdate = db.collection("users").doc(FirebaseAuth.instance.currentUser?.uid);
-                 userUpdate.update({"trailBlazer": chosenTrailBlazer, "trailBlazerName": trailBlazerName}).then(
-                   (value) =>   Navigator.push(
-                   context,
-                   MaterialPageRoute(
-                          builder: (context) => TrailBlazerProfile()
-                   )),
-                 );
-                 }
-               },
-               child: Text('Confirm'),
-             ),
-           ),   
-
-         ],
-         ),
-         ),
-         ]
-      )
-    )
-    );
+                    icon: Image.asset('assets/blue_type1.gif'),
+                    iconSize: 1,
+                    onPressed: () {
+                      chosenTrailBlazer = 'water1';
+                    },
+                  ),
+                )),
+                Expanded(
+                    child: SizedBox(
+                  height: 250,
+                  width: 250,
+                  child: IconButton(
+                    icon: Image.asset('assets/green_type1.gif'),
+                    iconSize: 1,
+                    onPressed: () {
+                      chosenTrailBlazer = 'grass1';
+                    },
+                  ),
+                )),
+              ],
+            ),
+            Container(
+                padding: const EdgeInsets.all(5),
+                child: SizedBox(
+                  height: 250,
+                  width: 250,
+                  child: IconButton(
+                    icon: Image.asset('assets/brown_type1.gif'),
+                    iconSize: 50,
+                    onPressed: () {
+                      chosenTrailBlazer = 'mountain1';
+                    },
+                  ),
+                )),
+            Center(
+                child: Text(
+              'Give your TrailBlazer a name:',
+              style: TextStyle(fontSize: 20),
+            )),
+            TextField(
+              onChanged: (value) {
+                trailBlazerName = value;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter name here',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (chosenTrailBlazer.isEmpty || trailBlazerName.isEmpty) {
+                    showAlertDialog(context);
+                  } else {
+                    final userUpdate = db
+                        .collection("users")
+                        .doc(FirebaseAuth.instance.currentUser?.uid);
+                    userUpdate.update({
+                      "trailBlazer": chosenTrailBlazer,
+                      "trailBlazerName": trailBlazerName
+                    }).then((value) {
+                      Navigator.of(context).pop();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => TrailBlazerProfile(),
+                        ),
+                      );
+                    });
+                  }
+                },
+                child: Text('Confirm'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ])));
   }
 }
- 
+
 showAlertDialog(BuildContext context) {
   // set up the button
   Widget okButton = TextButton(
