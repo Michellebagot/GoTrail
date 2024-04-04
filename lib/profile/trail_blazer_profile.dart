1e0d23477245b08d1b780a64d5452c9601f678a4
eleanor-rf <eleanorfrank@outlook.com>
2024-04-04 09:10:30 +0100

    add evolve trailblazer when navigating to trailblazer profile

--- lib/profile/trail_blazer_profile.dart
+++ lib/profile/trail_blazer_profile.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class TrailBlazerProfile extends StatefulWidget {
- 
   @override
   State<TrailBlazerProfile> createState() => _TrailBlazerProfileState();
 }
@@ -21,17 +20,20 @@ class _TrailBlazerProfileState extends State<TrailBlazerProfile> {
     fetchUserData();
   }
 
-void fetchUserData() async {
-  try {
-    var user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      var db = FirebaseFirestore.instance;
-      var data = await db.collection("users").doc(user.uid).get();
-      if (data.exists) {
-        setState(() {
-          userData = data.data();
-        });
-      }
+  void fetchUserData() async {
+    var db = FirebaseFirestore.instance;
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+      var userRef =
+          db.collection("users").doc(FirebaseAuth.instance.currentUser?.uid);
+      if (user != null) {
+        var db = FirebaseFirestore.instance;
+        var data = await db.collection("users").doc(user.uid).get();
+        if (data.exists) {
+          setState(() {
+            userData = data.data();
+          });
+        }
 
         if (userData['pointsEarned'] < 100) {
           pointsRequired = 100 - userData['pointsEarned'];
@@ -45,6 +47,24 @@ void fetchUserData() async {
         if (userData != null) {
           fetchtrailBlazerData(userData['trailBlazer']);
         }
+
+        if (userData['pointsEarned'] < 100) {
+          userRef.set({
+            "trailBlazer":
+                '${userData['trailBlazer'].substring(0, userData['trailBlazer'].length - 1)}1',
+          }, SetOptions(merge: true));
+        } else if (userData['pointsEarned'] >= 100 &&
+            userData['pointsEarned'] < 200) {
+          userRef.set({
+            "trailBlazer":
+                '${userData['trailBlazer'].substring(0, userData['trailBlazer'].length - 1)}2',
+          }, SetOptions(merge: true));
+        } else if (userData['pointsEarned'] >= 200) {
+          userRef.set({
+            "trailBlazer":
+                '${userData['trailBlazer'].substring(0, userData['trailBlazer'].length - 1)}3',
+          }, SetOptions(merge: true));
+        }
       } else {
         print("User document not found");
       }
@@ -52,7 +72,7 @@ void fetchUserData() async {
       print(e);
     }
   }
-  
+
   void fetchtrailBlazerData(trailBlazer) async {
     var db = FirebaseFirestore.instance;
     var data = await db.collection("trailBlazers").doc(trailBlazer).get();
@@ -63,76 +83,79 @@ void fetchUserData() async {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(   
+    return Scaffold(
       appBar: header(
-                context,
-                BoxConstraints(
-                  minWidth: double.infinity,
-                  maxWidth: double.infinity,
-                  minHeight: 30,
-                  maxHeight: 50,
-                ),
-                0),      
-     body: SingleChildScrollView(
-                child: Column(
-                          children: [
-                            userData != null && trailBlazerData != null ?
-            Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Center(
-                    child: Text('${userData['trailBlazerName']}',
-                    style: TextStyle(fontSize: 40.0),),
-                  ),
-              ),
-
-              Container(
-                decoration: BoxDecoration(
-                   image : DecorationImage(
-                    image: AssetImage('assets/background.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(20))
-                ),
-                margin: EdgeInsets.all(25.0),
-                child: Center(
-                  child: Image.asset('${trailBlazerData['path']}.gif') ,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                child: isFinalStage == false ? Center( 
-                  child: Text('Points Until Next Stage: $pointsRequired'),
-                ): Text('You have fully evolved'),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Center(
-                  child: Text('Current Points: ${userData['pointsEarned']}') ,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Center(
-                  child: Text('Type: ${trailBlazerData['type']}') ,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: Center(
-                  child: Text('Stage: ${trailBlazerData['stage']}') ,
-                ),
-              )
-            ],
-          )
-
-          : Center(child: CircularProgressIndicator()), // else Loading screen
-                          ],
-                       ),
-           ),
-      );
-   }
-}  
-
-
+          context,
+          BoxConstraints(
+            minWidth: double.infinity,
+            maxWidth: double.infinity,
+            minHeight: 30,
+            maxHeight: 50,
+          ),
+          0),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            userData != null && trailBlazerData != null
+                ? Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Center(
+                          child: Text(
+                            '${userData['trailBlazerName']}',
+                            style: TextStyle(fontSize: 40.0),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/background.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        margin: EdgeInsets.all(25.0),
+                        child: Center(
+                          child: Image.asset('${trailBlazerData['path']}.gif'),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 10),
+                        child: isFinalStage == false
+                            ? Center(
+                                child: Text(
+                                    'Points Until Next Stage: $pointsRequired'),
+                              )
+                            : Text('You have fully evolved'),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10.0),
+                        child: Center(
+                          child: Text(
+                              'Current Points: ${userData['pointsEarned']}'),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10.0),
+                        child: Center(
+                          child: Text('Type: ${trailBlazerData['type']}'),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10.0),
+                        child: Center(
+                          child: Text('Stage: ${trailBlazerData['stage']}'),
+                        ),
+                      )
+                    ],
+                  )
+                : Center(
+                    child: CircularProgressIndicator()), // else Loading screen
+          ],
+        ),
+      ),
+    );
+  }
+}
